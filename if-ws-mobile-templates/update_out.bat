fecb6cea782943b1b50a6e57f9657ef55a4c9123
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2011-09-01 18:49:30 -0400

    Started configuring installer build.

--- if-ws-mobile-templates/update_out.bat
+++
@@ -1,4 +0,0 @@
-copy websharper-mobile-offline-empty\websharper-mobile-offline-empty.zip out\websharper-mobile-offline-empty.zip
-copy websharper-mobile-offline-sample\websharper-mobile-offline-sample.zip out\websharper-mobile-offline-sample.zip
-copy websharper-mobile-online-empty\websharper-mobile-online-empty.zip out\websharper-mobile-online-empty.zip
-copy websharper-mobile-online-sample\websharper-mobile-online-sample.zip out\websharper-mobile-online-sample.zip
