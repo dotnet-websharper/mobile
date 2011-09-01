1e8a200f84a46bc205185d0431860505c07e0300
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2011-09-01 15:39:31 -0400

    Removed assemblyInfo.fs files.

--- ForTesting_WebSharperMobile/assemblyInfo.fs
+++
@@ -1,7 +0,0 @@
-﻿module assemblyInfo
-
-open System.Reflection
-
-[<assembly: AssemblyCompany "IntelliFactory">]
-[<assembly: AssemblyProduct "WebSharper Mobile SampleApp">]
-do ()
--- IntelliFactory.WebSharper.Mobile.Android/assemblyinfo.fs
+++
@@ -1,6 +0,0 @@
-﻿module assemblyinfo
-
-open System.Reflection
-
-[<assembly:AssemblyVersion "2.3.27.143">]
-do ()
