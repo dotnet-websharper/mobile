c63628e325233880f9c8cee00a0af5ccb1b0cc83
ramon.snir@IFBUD04.budapest.intellifactory.com <ramon.snir@IFBUD04.budapest.intellifactory.com>
2011-09-02 20:13:50 +0200

    Updated templates. VS2008 support.

---
+++ if-ws-mobile-templates/vs10/websharper-mobile-online-empty/WebSharperMobileProject/assemblyInfo.fs
@@ -0,0 +1,11 @@
+module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyProduct "WebSharper Mobile App">]
+[<assembly: AssemblyCompany "Company">]
+[<assembly: Guid "e7b1b3d8-05f0-45a2-8b04-81f0dec7e214">]
+[<assembly: AssemblyFileVersion "1.0.0.0">]
+[<assembly: AssemblyDescription "An app built using WebSharper Mobile">]
+do ()
